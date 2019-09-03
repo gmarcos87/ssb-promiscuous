867a674468c5c89f97ef394b1b719ca9b4e4b447
gmarcos87 <gmarcos87@gmail.com>
2019-09-03 14:22:46 -0300

    refactoring
    Added contributions and observations made by Mixmix and Andre in %6p2sPJY83IQRYAzWaNCXwYGvovlMK4ekEjiPvYwmSg=.sha256

--- index.js
+++ index.js
@@ -1,32 +1,43 @@
-var pull = require("pull-stream");
-var ma = require("multiserver-address");
+var pull = require("pull-stream")
 
-function addressToKey(address){
-    return `@${ma.decode(address).map(data => data[1].data[0])[0]}.ed25519`;
+var checkedList = []
+var waitingTime = 3000
+
+function isChecked(key) {
+  if (checkedList[key] && checkedList[key] > Date.now()) return true
+  checkedList[key] = Date.now() + waitingTime
+  return false
 }
 
 module.exports = {
-    name: "promiscuous",
-    version: "1.0.0",
-    manifest: {
-    },
-    init: (api) => {
-      pull(
-        api.lan.discoveredPeers(),
-        pull.drain(({address}) => {
-          var remoteKey = addressToKey(address)
-          api.conn.remember(address)
-          api.friends.isFollowing({ source: api.id, dest: remoteKey }, function(err, value) {
-            if (!err && !value) {
-              api.publish({
-                type: "contact",
-                contact: remoteKey,
-                following: true
-              }, console.log)
-            }
-          })
+  name: "promiscuous",
+  version: "1.0.1",
+  manifest: {},
+  init: (api) => {
+    if (!api.conn) throw new Error('ssb-promiscuous needs ssb-conn to be installed')
+    if (!api.friends) throw new Error('ssb-promiscuous needs ssb-friends to be installed')
+
+    pull(
+      api.conn.stagedPeers(),
+      pull.flatten(),
+      pull.drain(([addr, data]) => {
+        if (isChecked(data)) return
+        api.friends.isFollowing({ source: api.id, dest: data.key }, (err, value) => {
+          if (err) return console.error(err)
+          if (value === true) return
+          follow(data.key)
         })
-      )
-      return {}
+      })
+    )
+
+    return {}
+
+    function follow(feedId, cb = console.log) {
+      api.publish({
+        type: 'contact',
+        contact: feedId,
+        following: true
+      }, cb)
     }
   }
+}
